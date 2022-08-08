6cbc9322479f76c306715da38944621c5eefd633
LauraAlejandra2021 <3450@holbertonschool.com>
2022-08-07 21:25:59 -0500

    pep8 update

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """interview boxes"""
 
+
 def canUnlockAll(boxes):
-    """function that identifies 
+    """function that identifies
         if all the boxes have a key
         that opens them"""
     newlist = []
@@ -12,7 +13,7 @@ def canUnlockAll(boxes):
             return False
         for j in i:
             newlist.append(j)
-    
+
     for index, keys in enumerate(boxes):
         if index in newlist or index < k-1:
             return True
