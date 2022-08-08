5053d912c10e2a77f88d09d2a083906b4dc7512c
LauraAlejandra2021 <3450@holbertonschool.com>
2022-08-07 21:16:22 -0500

    update box

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,7 +12,7 @@ def canUnlockAll(boxes):
             return False
         for j in i:
             newlist.append(j)
-    print(newlist)
+    
     for index, keys in enumerate(boxes):
         if index in newlist or index < k-1:
             return True
